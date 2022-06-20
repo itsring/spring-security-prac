54112424ad2adb9e4e560614d9a2dfb1c5e0a7e2
psjCom <psj@bestco.kr>
2022-06-20 14:59:40 +0900

    add config
    컨피규 파일 추가

---
+++ src/main/java/com/psj/securityclone/config/PasswordEncoderConfig.java
@@ -0,0 +1,19 @@
+package com.psj.securityclone.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * PasswordEncoder Config
+ * DB에 비밀번호를 평문으로 저장하지 않기 때문에 암호화를 해서 넣어야 된다.
+ */
+@Configuration
+public class PasswordEncoderConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
