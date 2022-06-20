e3cc109df88b4b68be2a493a859d3a3a3fa1fabb
psjCom <psj@bestco.kr>
2022-06-20 14:24:10 +0900

    Initailize

---
+++ src/test/java/com/psj/securityclone/SecurityCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psj.securityclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
